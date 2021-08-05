fe1f873ef9a92fac9507ca7907b1039e3a91e559
Sanabel8 <sanabeljamal97@gmail.com>
2021-08-05 07:13:35 +0300

    finish lab9

---
+++ app/src/main/java/quotes/QuoteAPI.java
@@ -0,0 +1,35 @@
+package quotes;
+
+public class QuoteAPI {
+    private String quoteText;
+    private String quoteAuthor;
+
+    public QuoteAPI(String quoteText, String quoteAuthor) {
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return "QuoteAPI{" +
+                "quoteText='" + quoteText + '\'' +
+                ", quoteAuthor='" + quoteAuthor + '\'' +
+                '}';
+    }
+}
